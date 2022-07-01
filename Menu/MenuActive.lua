033b1c3989760496bcd9d8b564b373457d5077b8
Xatmo <42683376+Xatmo980@users.noreply.github.com>
2022-07-01 14:43:01 -0700

    Base Upload

---
+++ Menu/MenuActive.lua
@@ -0,0 +1,50 @@
+Menus["resurrect player"] = {
+    text = color.White.."Do you want to "..color.Gold..
+    "help\n"..color.White.."this person? it will cost you gold",
+    buttons = {						
+        { caption = "Patch them up.",
+            destinations = {
+                         menuHelper.destinations.setDefault("You lack the Gold"),
+                         menuHelper.destinations.setConditional("Success",
+                         {
+                          menuHelper.conditions.requireItem("gold_001", 500)
+                         },
+                         {
+                          menuHelper.effects.removeItem("gold_001", 500),
+	                      menuHelper.effects.runGlobalFunction("ActivatePlayer", "ResurrectPlayer", 
+			            {menuHelper.variables.currentPlayerDataVariable("targetPid")})
+                        })
+            }
+        },			
+        { caption = "Leave them there.", destinations = nil }
+    }
+}
+
+Menus["resurrect"] = {
+    text = color.Red .. "You are unconcious.\n" .. color.White .. "You can wait for another player\nor respawn at the nearest temple.",
+    buttons = {						
+        { caption = "Respawn",
+            destinations = {menuHelper.destinations.setDefault(nil,
+            { 
+				menuHelper.effects.runGlobalFunction(nil, "OnPlayerSendMessage",
+					{menuHelper.variables.currentPid(), "/resurrect"})
+                })
+            }
+        },			
+        { caption = "Wait", destinations = nil }
+    }
+}
+
+Menus["You lack the Gold"] = {
+    text = "You lack the required Gold.",
+    buttons = {
+        { caption = "Ok", destinations = nil }
+    }
+}
+
+Menus["Success"] = {
+    text = "500 Gold has been taken and they have been resurrected.",
+    buttons = {
+        { caption = "Ok", destinations = nil }
+    }
+}
---
+++ StatsInspect/StatsInspect.lua
@@ -0,0 +1,120 @@
+local config = {}
+local GuI = 44332210
+local ActivatePlayer = {}
+
+
+ActivatePlayer.ResurrectPlayer = function(pid)
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
+		tes3mp.Resurrect(tonumber(pid), 0)
+		local HealthBase = tes3mp.GetHealthBase(pid)
+		local P = (HealthBase * 50) / 100
+		tes3mp.SetHealthCurrent(pid, P)
+		tes3mp.SendStatsDynamic(pid)
+	end
+end
+
+ActivatePlayer.Res = function(pid)
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
+		Players[pid]:Resurrect()
+	end
+end
+
+ActivatePlayer.InspectPlayer = function(eventStatus, pid, cellDescription, objects, players)
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
+		local PlayerPid
+		for _, object in pairs(players) do
+			PlayerPid = object.pid
+		end	
+		if Players[PlayerPid] ~= nil and Players[PlayerPid]:IsLoggedIn() then		
+			Players[pid].data.targetPid = PlayerPid	
+			Players[PlayerPid].data.targetPid = pid	
+                        local player = Players[PlayerPid]	
+			local IsAlive = tes3mp.GetHealthCurrent(PlayerPid)	
+			if IsAlive ~= 0 then
+	                   local LvL = player.data.stats.level
+                           local Progress = player.data.stats.levelProgress
+                           local cHP = math.floor(player.data.stats.healthCurrent)
+                           local bHP = math.floor(player.data.stats.healthBase)
+                           local StatsList = "Level " .. LvL .. "\n" .. "Level Progress " .. Progress .. "\n" .. "HP " .. cHP .. "/" .. bHP
+                           tes3mp.CustomMessageBox(pid, GuI, StatsList, "Ok")
+                        else
+			   Players[pid].currentCustomMenu = "resurrect player"
+			   menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)		
+                          -- tes3mp.CustomMessageBox(pid, GuI, "This Dude is Dead Bro", "Ok")
+                        end
+
+		end
+	end
+end
+
+ActivatePlayer.ResurrectCheck = function(eventStatus, pid)
+	ActivatePlayer.ResurrectProcess(pid)
+	return customEventHooks.makeEventStatus(false,false)	
+end	
+
+ActivatePlayer.ResurrectProcess = function(pid)	
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
+		if DragonDoor then
+			DragonDoor.OnPlayerConnect(true, pid)
+		end	
+		local currentResurrectType
+		if config.respawnAtImperialShrine == true then
+			if config.respawnAtTribunalTemple == true then
+				if math.random() > 0.5 then
+					currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
+				else
+					currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
+				end
+			else
+				currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
+			end
+		elseif config.respawnAtTribunalTemple == true then
+			currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
+		elseif config.defaultRespawnCell ~= nil then
+			currentResurrectType = enumerations.resurrect.REGULAR
+			tes3mp.SetCell(pid, config.defaultRespawnCell)
+			tes3mp.SendCell(pid)
+			if config.defaultRespawnPos ~= nil and config.defaultRespawnRot ~= nil then
+				tes3mp.SetPos(pid, config.defaultRespawnPos[1], config.defaultRespawnPos[2], config.defaultRespawnPos[3])
+				tes3mp.SetRot(pid, config.defaultRespawnRot[1], config.defaultRespawnRot[2])
+				tes3mp.SendPos(pid)
+			end
+		end
+		local message = "You have been revived"
+		if currentResurrectType == enumerations.resurrect.IMPERIAL_SHRINE then
+			message = message .. " at the nearest shrine"
+		elseif currentResurrectType == enumerations.resurrect.TRIBUNAL_TEMPLE then
+			message = message .. " to the nearest temple"
+		end
+		message = message .. ".\n"
+		if Players[pid].data.shapeshift.isWerewolf == true then
+			Players[pid]:SetWerewolfState(false)
+		end
+		contentFixer.UnequipDeadlyItems(pid)
+		tes3mp.Resurrect(pid, currentResurrectType)
+		tes3mp.SendMessage(pid, message, false)
+	end
+end
+
+ActivatePlayer.OnDeathTime = function(eventStatus, pid)
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then 
+		return customEventHooks.makeEventStatus(false,true)
+	end
+end
+
+ActivatePlayer.DeathTimeExpiration = function(eventStatus, pid)
+	if eventStatus.validCustomHandlers then
+		if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
+			  Players[pid].currentCustomMenu = "resurrect"
+			  menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)		
+	        end
+	end
+end
+
+customEventHooks.registerHandler("OnObjectActivate", ActivatePlayer.InspectPlayer)
+customEventHooks.registerValidator("OnPlayerResurrect", ActivatePlayer.ResurrectCheck)
+customEventHooks.registerHandler("OnDeathTimeExpiration", ActivatePlayer.DeathTimeExpiration)
+customEventHooks.registerValidator("OnDeathTimeExpiration", ActivatePlayer.OnDeathTime)
+customCommandHooks.registerCommand("resurrect", ActivatePlayer.Res)
+
+return ActivatePlayer
