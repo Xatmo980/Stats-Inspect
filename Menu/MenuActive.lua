71f8ca86fb4de462f8642f43bcffb92a952bea93
Xatmo <42683376+Xatmo980@users.noreply.github.com>
2022-07-02 16:59:08 -0700

    Added an Easy Var to Change for Gold Cost

--- Menu/MenuActive.lua
+++ Menu/MenuActive.lua
@@ -1,16 +1,18 @@
+local GoldCost = 500 -- The amount of gold you want to charge for Resurrection
+
 Menus["resurrect player"] = {
     text = color.White.."Do you want to "..color.Gold..
-    "help\n"..color.White.."this person? it will cost you gold",
+    "help\n"..color.White.."this person? it will cost you " .. GoldCost .. " gold",
     buttons = {						
         { caption = "Patch them up.",
             destinations = {
                          menuHelper.destinations.setDefault("You lack the Gold"),
                          menuHelper.destinations.setConditional("Success",
                          {
-                          menuHelper.conditions.requireItem("gold_001", 500)
+                          menuHelper.conditions.requireItem("gold_001", GoldCost)
                          },
                          {
-                          menuHelper.effects.removeItem("gold_001", 500),
+                          menuHelper.effects.removeItem("gold_001", GoldCost),
 	                      menuHelper.effects.runGlobalFunction("ActivatePlayer", "ResurrectPlayer", 
 			            {menuHelper.variables.currentPlayerDataVariable("targetPid")})
                         })
@@ -43,7 +45,7 @@ Menus["You lack the Gold"] = {
 }
 
 Menus["Success"] = {
-    text = "500 Gold has been taken and they have been resurrected.",
+    text = GoldCost .. " Gold has been taken and they have been resurrected.",
     buttons = {
         { caption = "Ok", destinations = nil }
     }
