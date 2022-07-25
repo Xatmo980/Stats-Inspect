3856c28238379cb8d540f8101e8dfd2ae7369777
Xatmo <42683376+Xatmo980@users.noreply.github.com>
2022-07-25 03:47:26 -0700

    Rework Code - Removed Dependency for Menu Script
    Code has been Reworked to remove the need for the Menu Script

--- StatsInspect/StatsInspect.lua
+++ StatsInspect/StatsInspect.lua
@@ -1,15 +1,20 @@
 local config = {}
 local GuI = 44332210
+local DrGuI = 44332211
+local RGuI = 44332212
 local ActivatePlayer = {}
-
+local PlayerPid = ""
+local HealthPrecent = 50  -- Precent of Hp to Res With
+local Gold = 500          -- Amount to Charge a Player to Res another Player
 
 ActivatePlayer.ResurrectPlayer = function(pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		tes3mp.Resurrect(tonumber(pid), 0)
-		local HealthBase = tes3mp.GetHealthBase(pid)
-		local P = (HealthBase * 50) / 100
-		tes3mp.SetHealthCurrent(pid, P)
-		tes3mp.SendStatsDynamic(pid)
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
+		tes3mp.Resurrect(tonumber(PlayerPid), 0)
+		local HealthBase = tes3mp.GetHealthBase(PlayerPid)
+		local P = (HealthBase * HealthPrecent) / 100
+		tes3mp.SetHealthCurrent(PlayerPid, P)
+		tes3mp.SendStatsDynamic(PlayerPid)
+                PlayerPid = ""
 	end
 end
 
@@ -21,7 +26,6 @@ end
 
 ActivatePlayer.InspectPlayer = function(eventStatus, pid, cellDescription, objects, players)
 	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		local PlayerPid
 		for _, object in pairs(players) do
 			PlayerPid = object.pid
 		end	
@@ -47,10 +51,9 @@ ActivatePlayer.InspectPlayer = function(eventStatus, pid, cellDescription, objec
                                  end
                            local StatsList = "Level " .. LvL .. "\n" .. "Level Progress " .. Progress .. "\n" .. "HP " .. cHP .. "/" .. bHP .. "\n\n" .. "[MajorSkills]" .. "\n" .. MajN[0] .. "(" .. MaBase[0] .. ")" .. "\n" .. MajN[1] .. "(" .. MaBase[1] .. ")" .. "\n" .. MajN[2] .. "(" .. MaBase[2] .. ")" .. "\n" .. MajN[3] .. "(" .. MaBase[3] .. ")" .. "\n" .. MajN[4] .. "(" .. MaBase[4] .. "\n\n" .. "[MinorSkills]" .. "\n" .. MinN[0] .. "(" .. MiBase[0] .. ")" ..  "\n" .. MinN[1].. "(" .. MiBase[1] .. ")" .. "\n" .. MinN[2].. "(" .. MiBase[2] .. ")" .. "\n" .. MinN[3].. "(" .. MiBase[3] .. ")" .. "\n" .. MinN[4] .. "(" .. MiBase[4] .. ")"
                            tes3mp.CustomMessageBox(pid, GuI, StatsList, "Ok")
-                        else
-			   Players[pid].currentCustomMenu = "resurrect player"
-			   menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)		
-                          -- tes3mp.CustomMessageBox(pid, GuI, "This Dude is Dead Bro", "Ok")
+                           PlayerPid = ""
+                        else		
+                           tes3mp.CustomMessageBox(pid, RGuI, color.White.."Do you want to "..color.Gold.. "help\n"..color.White.."this person? it will cost you " .. Gold .. " gold", "Patch them up.;Leave them there.")
                         end
 
 		end
@@ -115,16 +118,75 @@ end
 ActivatePlayer.DeathTimeExpiration = function(eventStatus, pid)
 	if eventStatus.validCustomHandlers then
 		if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-			  Players[pid].currentCustomMenu = "resurrect"
-			  menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)		
+			  tes3mp.CustomMessageBox(pid, DrGuI, color.Red .. "You are unconcious.\n" .. color.White .. "You can wait for another player\nor respawn at the nearest temple.", "Respawn;Wait")	
 	        end
 	end
+end
+
+customEventHooks.registerHandler("OnGUIAction", function(eventStatus, pid, idGui, data)
+    local isValid = eventStatus.validDefaultHandler
+    if isValid ~= false then
+        if idGui == DrGuI then
+           if data ~= nil then
+              if tonumber(data) == 0 then
+                 ActivatePlayer.Res(pid)
+                 return
+              elseif tonumber(data) == 1 then
+                 return
+	      end
+           end
+        end
+        if idGui == RGuI then
+           if tonumber(data) == 0 then
+              if data ~= nil then
+                 if Gold <= ActivatePlayer.GoldGetAmount(pid) then
+                    ActivatePlayer.ResurrectPlayer(pid)
+                    ActivatePlayer.GoldSetAmount(pid,  ActivatePlayer.GoldGetAmount(pid) - Gold)
+                    tes3mp.MessageBox(pid, -1, Gold .. " Gold has been taken and they have been resurrected.")
+                    PlayerPid = ""
+                    return
+                 else
+                    tes3mp.MessageBox(pid, -1, "You lack the required Gold.")
+                    PlayerPid = ""
+                    return
+                 end
+             end
+           elseif tonumber(data) == 1 then
+              PlayerPid = ""
+              return
+	   end
+        end
+     end
+end)
+
+ActivatePlayer.GoldGetAmount = function(pid)
+    local goldIndex
+
+    if tableHelper.containsKeyValue(Players[pid].data.inventory, "refId", "gold_001", true) then
+        goldIndex = tableHelper.getIndexByNestedKeyValue(Players[pid].data.inventory, "refId", "gold_001")
+
+        return Players[pid].data.inventory[goldIndex].count
+    end
+
+    return 0
+end
+
+ActivatePlayer.GoldSetAmount = function(pid, g)
+    local goldIndex
+
+    if tableHelper.containsKeyValue(Players[pid].data.inventory, "refId", "gold_001", true) then
+        goldIndex = tableHelper.getIndexByNestedKeyValue(Players[pid].data.inventory, "refId", "gold_001")
+
+        Players[pid].data.inventory[goldIndex].count = g
+        Players[pid]:Save()
+        Players[pid]:LoadInventory()
+        Players[pid]:LoadEquipment()
+    end
 end
 
 customEventHooks.registerHandler("OnObjectActivate", ActivatePlayer.InspectPlayer)
 customEventHooks.registerValidator("OnPlayerResurrect", ActivatePlayer.ResurrectCheck)
 customEventHooks.registerHandler("OnDeathTimeExpiration", ActivatePlayer.DeathTimeExpiration)
 customEventHooks.registerValidator("OnDeathTimeExpiration", ActivatePlayer.OnDeathTime)
-customCommandHooks.registerCommand("resurrect", ActivatePlayer.Res)
 
 return ActivatePlayer
